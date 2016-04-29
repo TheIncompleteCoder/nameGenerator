1f548583268fc4d871488042f0e6ae968331c025
Akash Nagwanshi <akashnagwanshi@gmail.com>
2016-04-29 18:03:42 +0530

    Added files via upload

---
+++ name.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import random
+import os
+list1 = ['akash','amit','jay','rahul','anand','ravindra','ankit','kunal','saheb','arjun','aaditya','sanjay','op','sunil','navneet']
+listTemp = [] 
+os.system('clear')
+print "game started..."
+ 
+def game():
+ print "hit enter to continue:"
+ #raw_input()
+ print "player's name generated"
+ rand_item1 = random.sample(list1,1)  
+ rand_item2 = random.sample(list1,1)
+ listTemp.append(rand_item1)
+ listTemp.append(rand_item2)
+ try:
+    list1.remove(rand_item1)
+    list1.remove(rand_item2)
+ except:
+    pass
+ print rand_item1,
+ print " vs",
+ print rand_item2
+
+for i in list1:
+ game()
+ 
+for j in listTemp:
+ print j 
